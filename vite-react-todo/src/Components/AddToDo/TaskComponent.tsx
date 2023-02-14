5ece930ff1487cfe17b5edc2380564445288a8b7
Martin B <m.bogdanov9110@gmail.com>
2023-02-14 16:36:25 +0200

    added handlers for dlete confirm, cancel and show more info

--- vite-react-todo/src/Components/AddToDo/TaskComponent.tsx
+++ vite-react-todo/src/Components/AddToDo/TaskComponent.tsx
@@ -9,25 +9,29 @@ import { Task, taskState, deleteTask } from '../../features/tasks/taskSlice';
 
 const TaskComponent = (task: Task) => {
 
-
     const tasksState = useAppSelector(taskState);
     const dispatch = useAppDispatch();
 
     const [toDelete, setToDelete] = useState(false);
     const [toShowMoreInfo, settoShowMoreInfo] = useState(false);
 
+    const handleDeleteConfirm = () => dispatch(deleteTask(task.taskName));
+    const handleDeleteCancel = () => setToDelete(state => false);
+    const handleShowMoreInfo = () => settoShowMoreInfo(state => !state);
+
+
     return (
-        <div className={styles.taskItem} key={nanoid()}>
+        <div className={styles.taskItem}>
             <p className={styles.taskDetails}>{task.taskName}</p>
             <p className={styles.taskBody}>{task.taskBody}</p>
-            <button type='button' onClick={() => setToDelete(state => true)}>Delete task</button>
-            <button type='button' onClick={() => settoShowMoreInfo(state => !state)}>More info...</button>
+            <button type='button' onClick={() => setToDelete(state => !state)}>Delete task</button>
+            <button type='button' onClick={() => handleShowMoreInfo()}>More info...</button>
             {toDelete
                 ?
                 <div>
                     <p>Are you sure you want to delete this task?</p>
-                    <button onClick={() => dispatch(deleteTask(task.taskName))}> Yes</button>
-                    <button onClick={() => setToDelete(state => false)}>No</button>
+                    <button onClick={() => handleDeleteConfirm()}> Yes</button>
+                    <button onClick={() => handleDeleteCancel()}>No</button>
                 </div>
                 : null
             }
