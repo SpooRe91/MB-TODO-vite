59b6967ced944758f7e3ce74bb197b5b0d0b5cd8
Martin B <m.bogdanov9110@gmail.com>
2023-02-14 11:49:50 +0200

    moved the add form and task component to different files

--- vite-react-todo/src/Components/AddToDo/AddToDo.tsx
+++ vite-react-todo/src/Components/AddToDo/AddToDo.tsx
@@ -3,8 +3,9 @@ import { nanoid } from '@reduxjs/toolkit';
 
 import styles from "./AddToDo.module.scss";
 import { useAppSelector, useAppDispatch } from '../../App/hooks';
-import { addTask, taskState } from '../../features/tasks/taskSlice';
+import { taskState } from '../../features/tasks/taskSlice';
 import TaskComponent from './TaskComponent';
+import FormComponent from './FormComponent';
 
 
 const AddToDo = () => {
@@ -12,67 +13,10 @@ const AddToDo = () => {
     const tasksState = useAppSelector(taskState);
     const dispatch = useAppDispatch();
 
-    const [data, setData] = useState({ taskName: '', taskBody: '', taskDate: '', taskOwner: '' });
-
-    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-
-        setData(state => ({
-            ...state,
-            [e.target.name]: e.target.value
-        }));
-    }
-
-    const submitTask = (e: React.FormEvent<EventTarget>) => {
-        e.preventDefault();
-        dispatch(addTask(data));
-        setData({ taskName: '', taskBody: '', taskDate: '', taskOwner: '' });
-        console.log(data);
-    }
-
-    const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(e.target);
-        // dispatch(deleteTask())
-    }
-
     return (
 
         <div className={styles.main}>
-
-            <form className={styles.taskForm}>
-                <label htmlFor="taskName">Task name</label>
-                <input
-                    className={styles.inputField}
-                    type="text"
-                    id="taskName"
-                    name="taskName"
-                    onChange={(e) => changeHandler(e)}
-                    value={data.taskName}
-                    required
-                />
-
-                <label htmlFor="taskBody">Task body</label>
-                <input
-                    className={styles.inputField}
-                    type="text"
-                    id="taskBody"
-                    name="taskBody"
-                    onChange={(e) => changeHandler(e)}
-                    value={data.taskBody}
-                    required
-                />
-                <label htmlFor="taskBody">Task issued by</label>
-                <input
-                    className={styles.inputField}
-                    type="text"
-                    id="taskOwner"
-                    name="taskOwner"
-                    onChange={(e) => changeHandler(e)}
-                    value={data.taskOwner}
-                    required
-                />
-                <input type="submit" onClick={(e) => submitTask(e)} />
-            </form>
-
+            <FormComponent />
             <div className={styles.newTasksContaner}>
                 <h2>New Tasks</h2>
                 {
@@ -85,7 +29,8 @@ const AddToDo = () => {
                                     taskBody={task.taskBody}
                                     taskStart={task.taskStart}
                                     taskOwner={task.taskOwner}
-                                    taskId={task.taskId} />
+                                    taskId={task.taskId}
+                                    error={task.error} />
                             )
                         })
                         :
