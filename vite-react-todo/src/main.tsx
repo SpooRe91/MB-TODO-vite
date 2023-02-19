3f155d3138071a92da7007d69249da3427543411
Martin B <m.bogdanov9110@gmail.com>
2023-02-19 14:55:37 +0200

    imported reset CSS

--- vite-react-todo/src/main.tsx
+++ vite-react-todo/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { store } from "./App/store";
 import App from './App'
-import './index.scss'
+import './index.scss';
+import './ResetCSS.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
