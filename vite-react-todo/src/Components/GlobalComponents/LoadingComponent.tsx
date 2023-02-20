1200480d039810b72ecc6384ae88291f214ba8ad
Martin B <m.bogdanov9110@gmail.com>
2023-02-20 02:03:24 +0200

    moved LoadingComponent to separate folder

---
+++ vite-react-todo/src/Components/GlobalComponents/LoadingComponent.tsx
@@ -0,0 +1,19 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+import BarLoader from "react-spinners/BarLoader";
+
+import styles from "../AddToDo/AddToDo.module.scss";
+
+const LoadingComponent = (props:
+    DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>) => {
+
+    return (
+        <div className={styles.loaderComp}>
+            <BarLoader
+                width={"100%"}
+                color={'coral'}
+            />
+        </div>
+    )
+}
+
+export default LoadingComponent
