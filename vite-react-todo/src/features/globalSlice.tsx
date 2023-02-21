5a15e54a7d03cb3d19290485601b057ef7ed60b4
Martin B <m.bogdanov9110@gmail.com>
2023-02-21 16:28:49 +0200

    added a global show form method to the globalSlice

--- vite-react-todo/src/features/globalSlice.tsx
+++ vite-react-todo/src/features/globalSlice.tsx
@@ -4,11 +4,13 @@ import type { RootState } from '../App/store';
 export interface ILoading {
     loading: boolean;
     toDelete: boolean;
+    showForm: boolean;
 }
 
 const initialState: ILoading = {
     loading: false,
-    toDelete: false
+    toDelete: false,
+    showForm: false
 }
 
 export const globalActions = createSlice({
@@ -22,10 +24,14 @@ export const globalActions = createSlice({
         setToDelete: (state, action) => {
             state.loading = action.payload.loading;
             state.toDelete = action.payload.toDelete;
+        },
+
+        setToShowForm: (state, action) => {
+            state.showForm = action.payload;
         }
     }
 });
 
-export const { setLoading, setToDelete } = globalActions.actions;
+export const { setLoading, setToDelete, setToShowForm } = globalActions.actions;
 export const globalState = (state: RootState) => state.globalSlice;
 export default globalActions.reducer;
