56e574aa4babed1b0cb2c25d04785e0d78a2cdd8
Martin B <m.bogdanov9110@gmail.com>
2023-06-02 16:23:23 +0300

    UPDATE: Removed show form button from APP, added it to NavBar + styles for responsive

--- vite-react-todo/src/App.tsx
+++ vite-react-todo/src/App.tsx
@@ -3,32 +3,22 @@ import './ResetCSS.scss';
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { globalState, setToShowForm } from './features/globalSlice';
-import { useAppDispatch, useAppSelector } from './App/hooks';
+import { globalState } from './features/globalSlice';
+import { useAppSelector } from './App/hooks';
 
-import LoadingComponent from './Components/GlobalComponents/LoadingComponent';
-import CompletedComponent from './Components/CompletedComponent/CompletedComponent';
-import NavBar from './Components/NavBar/NavBar';
-import AddToDo from './Components/AddToDo/AddToDo';
-import { MdFormatListNumbered } from 'react-icons/md';
+import { NavBar, LoadingComponent } from './allComponentsImported';
+import HomeComp from './Components/HomeComponent/HomeComp';
 
 const FormComponent = lazy(() => import('./Components/FormComponent/FormComponent'));
+const AddToDo = lazy(() => import('./Components/AddToDo/AddToDo'));
+const CompletedComponent = lazy(() => import('./Components/CompletedComponent/CompletedComponent'));
 
 function App() {
   const globalStateData = useAppSelector(globalState);
-  const dispatch = useAppDispatch();
 
   return (
     <main data-cy="app-main">
       <NavBar />
-      <div>
-        <button
-          className='showFormButton'
-          data-cy='showFormButton'
-          onClick={() => dispatch(setToShowForm(!globalStateData.showForm))}>
-          <MdFormatListNumbered /> {globalStateData.showForm ? 'Hide task from' : 'Show task form'}
-        </button>
-      </div>
       {
         globalStateData.loading
           ?
@@ -46,12 +36,11 @@ function App() {
             null
         }
         <Routes>
+          <Route path='/' element={<HomeComp />} />
           <Route path='/all-tasks' element={<AddToDo />} />
           <Route path='/done-deleted-tasks' element={<CompletedComponent />} />
         </Routes>
       </Suspense>
-      {/* <Suspense fallback={<LoadingComponent />}>
-        </Suspense> */}
     </main >
   )
 }
