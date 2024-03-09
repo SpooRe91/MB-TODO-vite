ee28399f0048186fe98ea5d82a6505506424afa7
Martin B <m.bogdanov9110@gmail.com>
2024-03-09 22:45:59 +0200

    fix: added imports to index file, fixed formatting + small style 1

--- vite-react-todo/cypress/component/ComponentName.cy.tsx
+++ vite-react-todo/cypress/component/ComponentName.cy.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import NavBar from "../../src/Components/NavBar/NavBar";
-describe('ComponentName.cy.tsx', () => {
-  it('playground', () => {
-    <NavBar />
-  })
-})
+import { NavBar } from "../../src/Components/index";
+describe("ComponentName.cy.tsx", () => {
+    it("playground", () => {
+        <NavBar />;
+    });
+});
