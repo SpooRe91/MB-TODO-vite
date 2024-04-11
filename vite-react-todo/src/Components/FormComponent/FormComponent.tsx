04dabcba2140b2adc4977d0873295359130cc723
Martin B <85784810+SpooRe91@users.noreply.github.com>
2024-04-12 02:15:37 +0300

    fix:fixed styles of some components + Form submit logic (#7)

--- vite-react-todo/src/Components/FormComponent/FormComponent.tsx
+++ vite-react-todo/src/Components/FormComponent/FormComponent.tsx
@@ -1,48 +1,45 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from "react";
 
 import styles from "./FormComponent.module.scss";
-import {
-    MdOutlineDescription,
-    MdGrading,
-    MdPersonPin,
-    MdDateRange,
-} from "react-icons/md";
+import { MdOutlineDescription, MdGrading, MdPersonPin, MdDateRange } from "react-icons/md";
 import { BsFillCheckCircleFill } from "react-icons/bs";
-import { useAppSelector, useAppDispatch } from '../../App/hooks';
-import { taskState, addTask, editTask } from '../../features/tasks/taskSlice';
-import { globalState, setToEditTask, setToShowForm } from '../../features/globalSlice';
-import { nanoid } from '@reduxjs/toolkit';
+import { useAppSelector, useAppDispatch } from "../../App/hooks";
+import { taskState, addTask, editTask } from "../../features/tasks/taskSlice";
+import { globalState, setToEditTask, setToShowForm } from "../../features/globalSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
 const FormComponent = () => {
-
     const globalStateData = useAppSelector(globalState);
     const tasksState = useAppSelector(taskState);
     const dispatch = useAppDispatch();
 
-    const [errorMessage, setErrorMessage] = useState('');
-    const [addedMessage, setaddedMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState("");
+    const [addedMessage, setaddedMessage] = useState("");
 
-    const [data, setData] = useState(globalStateData.taskToEdit ? globalStateData.taskToEdit : {
-        taskName: '',
-        taskBody: '',
-        taskStart: '',
-        taskEnd: '',
-        taskOwner: '',
-        taskId: ''
-    });
+    const [data, setData] = useState(
+        globalStateData.taskToEdit
+            ? globalStateData.taskToEdit
+            : {
+                  taskName: "",
+                  taskBody: "",
+                  taskStart: "",
+                  taskEnd: "",
+                  taskOwner: "",
+                  taskId: "",
+              }
+    );
 
     const handleHideContainer = (e: React.MouseEvent<HTMLSpanElement>) => {
-        e.target === e.currentTarget
-            ? (dispatch(setToShowForm(!globalStateData.showForm)))
-            : null
+        e.target === e.currentTarget ? dispatch(setToShowForm(!globalStateData.showForm)) : null;
     };
 
-    const changeHandler = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-
-        setErrorMessage(state => '');
-        setData(state => ({
+    const changeHandler = (
+        e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        setErrorMessage("");
+        setData((state) => ({
             ...state,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         }));
     };
 
@@ -50,30 +47,63 @@ const FormComponent = () => {
         e.preventDefault();
 
         if (!errorMessage) {
+            if (tasksState.tasks?.find((task) => task.taskName === data.taskName)) {
+                return setErrorMessage("This name already exists!");
+            }
 
             if (globalStateData.taskToEdit) {
                 dispatch(editTask(data));
-                setaddedMessage(state => 'Task edited!');
-                dispatch(setToEditTask(null))
-            } else {
-                if (tasksState.tasks?.find(task => task.taskName === data.taskName)) {
-                    return setErrorMessage(state => 'This name already exists!');
-                }
-
-                dispatch(addTask({ ...data, taskId: nanoid() }));
-                setaddedMessage(state => 'Task added!');
+                setaddedMessage("Task edited!");
+                dispatch(setToEditTask(null));
+                return;
             }
-            setData({ taskName: '', taskBody: '', taskStart: '', taskEnd: '', taskOwner: '', taskId: '' });
+
+            dispatch(addTask({ ...data, taskId: nanoid() }));
+            setaddedMessage("Task added!");
+
+            setData({ taskName: "", taskBody: "", taskStart: "", taskEnd: "", taskOwner: "", taskId: "" });
         }
     };
 
     const handleAddedMessage = () => {
-        setaddedMessage(state => '');
+        setaddedMessage("");
     };
+
+    const renderFormButtons = useMemo(
+        () =>
+            globalStateData.taskToEdit ? (
+                <button type="submit" className={styles.submitBtn}>
+                    Edit
+                    <BsFillCheckCircleFill />
+                </button>
+            ) : (
+                <button type="submit" className={styles.submitBtn} data-cy="submit-button">
+                    Submit
+                    <BsFillCheckCircleFill />
+                </button>
+            ),
+        []
+    );
+
+    const renderNotification = useMemo(
+        () => (
+            <div className={styles.addedContainer} data-cy="added-message">
+                <p>{addedMessage}</p>
+                <button onClick={() => handleAddedMessage()} data-cy="added-ok-button">
+                    OK
+                </button>
+            </div>
+        ),
+        [addedMessage]
+    );
 
     return (
         <section className={styles.formContainer} data-cy="form-component">
-            <span className={styles.back} onClick={(e) => handleHideContainer(e)} data-cy="show-hide-background"></span>
+            <span
+                className={styles.back}
+                onClick={(e) => handleHideContainer(e)}
+                data-cy="show-hide-background"
+            ></span>
 
             <form className={styles.taskForm} onSubmit={(e) => submitTask(e)}>
                 <h3>Add task form</h3>
@@ -88,22 +118,26 @@ const FormComponent = () => {
                         type="text"
                         id="taskName"
                         name="taskName"
-                        placeholder='Task Name...'
+                        placeholder="Task Name..."
                         onChange={(e) => changeHandler(e)}
                         value={data.taskName}
                         required
                         data-cy="taskName"
                     />
                 </div>
-                <p className={styles.errorMessage} data-cy="error-name-already-exists">{errorMessage ? errorMessage : null}</p>
+                <p className={styles.errorMessage} data-cy="error-name-already-exists">
+                    {errorMessage ? errorMessage : null}
+                </p>
                 <div className={styles.inputContainer}>
-                    <label htmlFor="taskBody"><MdGrading className={styles.icon} /> Task body*</label>
+                    <label htmlFor="taskBody">
+                        <MdGrading className={styles.icon} /> Task body*
+                    </label>
                     <textarea
                         className={styles.inputField}
                         style={{ background: data.taskBody ? "green" : "#830000" }}
                         id="taskBody"
                         name="taskBody"
-                        placeholder='Task description...'
+                        placeholder="Task description..."
                         onChange={(e) => changeHandler(e)}
                         value={data.taskBody}
                         required
@@ -111,14 +145,16 @@ const FormComponent = () => {
                     />
                 </div>
                 <div className={styles.inputContainer}>
-                    <label htmlFor="taskOwner"><MdPersonPin className={styles.icon} /> Task issued by*</label>
+                    <label htmlFor="taskOwner">
+                        <MdPersonPin className={styles.icon} /> Task issued by*
+                    </label>
                     <input
                         className={styles.inputField}
                         style={{ background: data.taskOwner ? "green" : "#830000" }}
                         type="text"
                         id="taskOwner"
                         name="taskOwner"
-                        placeholder='John Smith...'
+                        placeholder="John Smith..."
                         onChange={(e) => changeHandler(e)}
                         value={data.taskOwner}
                         required
@@ -126,72 +162,45 @@ const FormComponent = () => {
                     />
                 </div>
                 <div className={styles.inputContainer}>
-                    <label htmlFor="taskStart"><MdDateRange className={styles.icon} /> Task start date*</label>
+                    <label htmlFor="taskStart">
+                        <MdDateRange className={styles.icon} /> Task start date*
+                    </label>
                     <input
                         className={styles.inputField}
                         style={{ background: data.taskStart ? "green" : "#830000" }}
                         type="date"
                         id="taskStart"
                         name="taskStart"
-                        placeholder='01.01.2023...'
-                        required pattern="\d{2}-\d{2}-\d{4}"
+                        placeholder="01.01.2023..."
+                        required
+                        pattern="\d{2}-\d{2}-\d{4}"
                         onChange={(e) => changeHandler(e)}
                         value={data.taskStart}
                         data-cy="taskStart"
                     />
                 </div>
                 <div className={styles.inputContainer}>
-                    <label htmlFor="taskEnd"><MdDateRange className={styles.icon} /> Task end date*</label>
+                    <label htmlFor="taskEnd">
+                        <MdDateRange className={styles.icon} /> Task end date*
+                    </label>
                     <input
                         className={styles.inputField}
                         style={{ background: data.taskEnd ? "green" : "#830000" }}
                         type="date"
                         id="taskEnd"
                         name="taskEnd"
-                        placeholder='01.01.2023...'
-                        required pattern="\d{2}-\d{2}-\d{4}"
+                        placeholder="01.01.2023..."
+                        required
+                        pattern="\d{2}-\d{2}-\d{4}"
                         onChange={(e) => changeHandler(e)}
                         value={data.taskEnd}
                         data-cy="taskEnd"
                     />
                 </div>
-                {
-                    globalStateData.taskToEdit
-                        ?
-                        <button
-                            type="submit"
-                            className={styles.submitBtn}>
-                            Edit
-                            <BsFillCheckCircleFill />
-                        </button>
-                        :
-                        <button
-                            type="submit"
-                            className={styles.submitBtn}
-                            data-cy="submit-button">
-                            Submit
-                            <BsFillCheckCircleFill />
-                        </button>
-                }
+                {addedMessage ? renderNotification : renderFormButtons}
             </form>
-            {
-                addedMessage
-                    ?
-                    <div
-                        className={styles.addedContainer}
-                        data-cy="added-message"
-                    >
-                        <p>{addedMessage}</p>
-                        <button
-                            onClick={() => handleAddedMessage()}
-                            data-cy="added-ok-button">
-                            OK
-                        </button>
-                    </div>
-                    : null
-            }
         </section>
-    )
-}
+    );
+};
 
-export default FormComponent
+export default FormComponent;
